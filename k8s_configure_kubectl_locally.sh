1a6176360f0632ddb92f5df23017f14a34a465dc
Andrea Zonca <code@andreazonca.com>
2022-08-22 15:50:11 -0700

    be prepared in case user has a CIDR with 10.0.x.x instead of 10.0.0.x

--- k8s_configure_kubectl_locally.sh
+++ k8s_configure_kubectl_locally.sh
@@ -1,2 +1,1 @@
-#sed 's/10\.0\.0\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf > $HOME/.kube/config
-sed -i 's/10\.0\.0\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf #> $HOME/.kube/config
+sed -i 's/10\.0\.[[:digit:]]\.[[:digit:]]\+/'"$IP/g" inventory/$CLUSTER/artifacts/admin.conf
