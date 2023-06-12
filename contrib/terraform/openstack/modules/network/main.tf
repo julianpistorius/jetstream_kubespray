0e297f41d2e0e10aadbc9fd36aef2c7a60fad526
Andrea Zonca <code@andreazonca.com>
2023-06-12 11:13:14 -0700

    hardcode network id

--- contrib/terraform/openstack/modules/network/main.tf
+++ contrib/terraform/openstack/modules/network/main.tf
@@ -21,7 +21,7 @@ resource "openstack_networking_network_v2" "k8s" {
 resource "openstack_networking_subnet_v2" "k8s" {
   name            = "${var.cluster_name}-internal-network"
   count           = var.use_neutron
-  network_id      = openstack_networking_network_v2.k8s[count.index].id
+  network_id      = "bdd21138-b4d2-4e2d-8114-efaf248c3722"
   subnetpool_id   = var.subnetpool_id
   ip_version      = 4
   dns_nameservers = var.dns_nameservers
